7962df93fde6f410528c9b51f537ea4d705a95a9
luongthetai2705 <luongthetai2705@gmail.com>
2025-10-06 21:04:03 +0700

    Buổi 2: Kiểu dữ liệu và toán tử

---
+++ Buoi2/script.js
@@ -0,0 +1,24 @@
+// let userName = "Hello world";
+// let age = 25;
+// const pi = 3.14;
+
+// let address = null;
+
+// const isHandsome = true;
+
+// let a;
+
+// console.log("Ban co dep trai khong: " + isHandsome);
+// console.log(age);
+// console.log(pi);
+// console.log(address);
+
+// let a = "10";
+// let b = 20;
+
+// let c = b + a;
+
+// console.log(c)
+
+let data = prompt("Hãy nhập số gì đó: ")
+alert(data)
