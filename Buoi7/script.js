feb54cdd8e6cf0bc7d4e7401c125e8fb4bce1dbe
luongthetai2705 <luongthetai2705@gmail.com>
2025-10-14 21:06:53 +0700

    Buổi 7: Đối tượng (Object)

---
+++ Buoi7/script.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "The Tai",
+    age: 25,
+    gender: "male"
+}
+
+student['address'] = "HCM city"
+student['name'] = "Messi"
+delete student['gender']
+
+console.log(student)
